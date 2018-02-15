7bc49b485470c4d5d754199b90cd8c8dac5edbfe
Ubuntu <ubuntu@ip-172-31-20-146.us-west-2.compute.internal>
2018-02-15 17:14:55 +0000

    update ignore

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export MONGO_URL=mongodb://iotnet:11akers32@35.227.149.62:27017/iotnet
-meteor --port=3006 --settings=settings.json
+#export MONGO_URL=mongodb://iotnet:11akers32@35.227.149.62:27017/iotnet
+meteor --port=3000 --settings=settings.json
